a5cb3c3fbba105b2a39ca5b34209b3ac6c867106
dhananjaypai08 <dhananjay2002pai@gmail.com>
2025-02-09 18:36:56 +0530

    rust dependency fix

--- stylus-contracts/src/lib.rs
+++ stylus-contracts/src/lib.rs
@@ -1,44 +1,49 @@
-// src/lib.rs
-#![cfg_attr(not(any(feature = "export-abi", test)), no_main)]
+#![cfg_attr(not(feature = "export-abi"), no_main)]
 extern crate alloc;
 
 mod erc721;
-mod barn_nft;
 
+use self::erc721::{Erc721, Erc721Params, Erc721Error};
 use alloy_primitives::{Address, U256};
-use stylus_sdk::{msg, prelude::*};
-use crate::barn_nft::{BaseArena, BARNNFT};
-use crate::erc721::Erc721Error;
+use stylus_sdk::{prelude::*, storage::StorageU256};
 
-sol_storage! {
-    #[entrypoint]
-    pub struct BaseArenaContract {
-        #[borrow]
-        BaseArena base_arena;
+// Remove the global allocator as it's provided by stylus-sdk
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+pub struct SimpleNftParams;
+impl Erc721Params for SimpleNftParams {
+    const NAME: &'static str = "SimpleNFT";
+    const SYMBOL: &'static str = "SNFT";
+
+    fn token_uri(token_id: U256) -> String {
+        format!("https://my-nft-metadata.com/{}.json", token_id)
     }
 }
 
-#[public]
-impl BaseArenaContract {
-    pub fn safe_mint(
-        &mut self,
-        rewards_earned_user: U256,
-        uri: String,
-        doppleganger_uri: String,
-        to: Address,
-    ) -> Result<(), Erc721Error> {
-        self.base_arena.safe_mint(rewards_earned_user, uri, doppleganger_uri, to)
+sol_storage! {
+    #[entrypoint]
+    pub struct SimpleNFT {
+        #[borrow]
+        Erc721<SimpleNftParams> erc721;
+        StorageU256 next_token_id;
+    }
+}
+
+#[external]
+impl SimpleNFT {
+    pub fn mint(&mut self, to: Address) -> Result<U256, Erc721Error> {
+        let token_id = self.next_token_id.get();
+        self.next_token_id.set(token_id + U256::from(1));
+        self.erc721.mint(to)?;
+        Ok(token_id)
     }
 
-    pub fn get_all_users(&self) -> Vec<Address> {
-        self.base_arena.get_all_users()
+    pub fn owner_of(&self, token_id: U256) -> Result<Address, Erc721Error> {
+        self.erc721.owner_of(token_id)
     }
 
-    pub fn save_response(&mut self, user: Address, data: String) {
-        self.base_arena.save_response(user, data);
-    }
-
-    pub fn get_nfts(&self, user: Address) -> Vec<BARNNFT> {
-        self.base_arena.get_nfts(user)
+    pub fn token_uri(&self, token_id: U256) -> Result<String, Erc721Error> {
+        self.erc721.token_uri(token_id)
     }
 }
